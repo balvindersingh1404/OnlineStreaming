9e1279bb753868fd8335bb95c5dbbb2157887368
Prosanto <pro833020>
2017-03-21 23:15:49 +0600

    new source code 22aaaa8

--- app/src/main/java/com/headsupseven/corp/NotificaionvideoActiivty.java
+++ app/src/main/java/com/headsupseven/corp/NotificaionvideoActiivty.java
@@ -1,5 +1,6 @@
 package com.headsupseven.corp;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
+import com.headsupseven.corp.customview.VideoView;
 
 
 /**
@@ -16,6 +18,7 @@ import android.widget.ImageView;
  */
 
 public class NotificaionvideoActiivty extends AppCompatActivity {
+    private VideoView video_view ;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,7 +48,7 @@ public class NotificaionvideoActiivty extends AppCompatActivity {
 
     private void responseDataShow() {
 
-        String responseData = "http://54.254.199.253:8080/recorded/live/IMG0001.1488825900503766746.MP4";
+        String responseData = "http://d1exh2z1dm71tm.cloudfront.net/uploaded/A14857078588661489939238012947800.1489939222621592816.FLV";
         Uri uri = Uri.parse(responseData);
         ImageView upview = (ImageView) this.findViewById(R.id.upview);
         upview.setOnClickListener(new View.OnClickListener() {
@@ -54,6 +57,18 @@ public class NotificaionvideoActiivty extends AppCompatActivity {
                 jumpMain();
             }
         });
+
+        video_view=(VideoView)this.findViewById(R.id.video_view);
+        video_view.setVideoURI(uri);
+//        video_view.setVideoPath(responseData);
+        video_view.requestFocus();
+        video_view.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            // Close the progress bar and play the video
+            public void onPrepared(MediaPlayer mp) {
+                video_view.start();
+            }
+        });
+
     }
 
     private synchronized void jumpMain() {
